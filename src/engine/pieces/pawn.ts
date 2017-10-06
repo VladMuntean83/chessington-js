2cd2e3c35482361f54e87a538e4e1636e6c26760
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:33:06 +0100

    Added a test that pawns cannot move through other pieces

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -17,22 +17,34 @@ export default class Pawn extends Piece {
 
             const color_dir = this.player == Player.WHITE ? 1 : -1;
 
+            if (board.getPiece(new Square(currSquare.row + color_dir, currSquare.col)) != undefined)
+               return [];
+
             moves.push({
                 'row': currSquare.row + color_dir,
                 'col': currSquare.col
             });
 
-            if (this.player == Player.WHITE && currSquare.row == 1)
+            if (this.player == Player.WHITE && currSquare.row == 1) {
+
+                if (board.getPiece(new Square(currSquare.row + 2, currSquare.col)) != undefined)
+                    return moves;
+
                 moves.push({
                     'row': currSquare.row + 2,
                     'col': currSquare.col
-                })
+                });
+            }
 
-            else if (this.player == Player.BLACK && currSquare.row == 6)
+            else if (this.player == Player.BLACK && currSquare.row == 6) {
+                if (board.getPiece(new Square(currSquare.row - 2, currSquare.col)) != undefined)
+                    return moves;
+
                 moves.push({
                     'row': currSquare.row - 2,
                     'col': currSquare.col
-                })
+                });
+            }
 
         } catch (e) {
             console.error(e);
