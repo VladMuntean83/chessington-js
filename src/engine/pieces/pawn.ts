9bef2bd54b33c29f29d703dac21c4f929cdf97a4
Vlad Muntean <Vlad.Muntean@softwire.com>
2025-07-07 11:08:15 +0300

    Fixed board declaration and pawn move functionality

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,18 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
+        try {
+            const currSquare: Square = board.findPiece(this);
+
+            const color_dir = this.player == Player.WHITE ? 1 : -1;
+
+            return {
+                'row': currSquare.row + color_dir,
+                'col': currSquare.col
+            };
+        } catch (e) {
+            console.error(e);
+        }
         return new Array(0);
     }
 }
