0bd9587fe4fa3358fcdf16739e9956d4b9e77d62
Vlad Muntean <Vlad.Muntean@softwire.com>
2025-07-07 14:25:06 +0300

    Added a test that rooks can't move through pieces

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -14,17 +15,27 @@ export default class Rook extends Piece {
             const currentSquare = board.findPiece(this);
 
             for (let i = 0; i < 8; i++) {
-                if (i != currentSquare.row)
+                if (i != currentSquare.row) {
+
+                    if (board.getPiece(new Square(i, currentSquare.col)) != undefined)
+                        break;
+
                     moves.push({
                         'row': i,
                         'col': currentSquare.col
                     });
+                }
 
-                if (i != currentSquare.col)
+                if (i != currentSquare.col) {
+
+                    if (board.getPiece(new Square(currentSquare.row, i)) != undefined)
+                        break;
+
                     moves.push({
                         'row': currentSquare.row,
                         'col': i
                     });
+                }
             }
         } catch (e) {
             console.error(e);
