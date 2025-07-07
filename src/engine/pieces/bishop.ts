852dc38ca916e5c0622544bc74df25e22b56d6b8
Vlad Muntean <Vlad.Muntean@softwire.com>
2025-07-07 12:30:18 +0300

    Add bishop move and bound checking

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,42 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+
+        const moves: Object[] = [];
+        try {
+            const currentSquare: Square = board.findPiece(this);
+
+            const dimDist: number = Math.min(currentSquare.row, currentSquare.col);
+
+            let curr_row: number = currentSquare.row - dimDist;
+            let curr_col: number = currentSquare.col - dimDist;
+
+            while (curr_row < 8 && curr_col < 8) {
+                if (curr_row != currentSquare.row && Piece.inBounds(curr_row, curr_col))
+                    moves.push({
+                        'row': curr_row,
+                        'col': curr_col
+                    })
+                curr_col++;
+                curr_row++;
+            }
+
+            curr_row = currentSquare.row - dimDist;
+            curr_col = currentSquare.col + dimDist;
+
+            while (curr_row < 8 && curr_col > -1) {
+                if (curr_row != currentSquare.row && Piece.inBounds(curr_row, curr_col))
+                    moves.push({
+                        'row': curr_row,
+                        'col': curr_col
+                    });
+                curr_row++;
+                curr_col--;
+            }
+        } catch (e) {
+            console.error(e);
+        }
+
+        return moves;
     }
 }
--- src/engine/pieces/piece.ts
+++ src/engine/pieces/piece.ts
@@ -13,6 +13,10 @@ export default class Piece {
         throw new Error('This method must be implemented, and return a list of available moves');
     }
 
+    public static inBounds(x: number, y: number): boolean {
+        return x >= 0 && y >= 0 && x < 8 && y < 8;
+    }
+
     public moveTo(board: Board, newSquare: Square) {
         const currentSquare = board.findPiece(this);
         board.movePiece(currentSquare, newSquare);
