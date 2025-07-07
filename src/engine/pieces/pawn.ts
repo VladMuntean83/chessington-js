ff46ab40220e5ccd9cc32f029fcd0ae8af008807
Vlad Muntean <Vlad.Muntean@softwire.com>
2025-07-07 16:13:24 +0300

    Added taking for pawns, both colors

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -2,10 +2,30 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from "../square";
+import King from "./king";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
         super(player);
+    }
+
+    private static checkTake(color_dir: number, row: number,
+                             col:number, board:Board, moves: Square[]) {
+        if (Piece.inBounds(row + color_dir, col + 1)) {
+            const obstacle: (Piece | undefined) = board.getPiece(new Square(row + color_dir, col + 1));
+
+            if (obstacle != undefined)
+                if(!(obstacle instanceof King) && obstacle.player != board.currentPlayer)
+                    moves.push(new Square(row + color_dir, col + 1));
+        }
+
+        if (Piece.inBounds(row + color_dir, col - 1)) {
+            const obstacle: (Piece | undefined) = board.getPiece(new Square(row + color_dir, col - 1));
+
+            if (obstacle != undefined)
+                if(!(obstacle instanceof King) && obstacle.player != board.currentPlayer)
+                    moves.push(new Square(row + color_dir, col - 1));
+        }
     }
 
     public getAvailableMoves(board: Board) {
@@ -16,6 +36,8 @@ export default class Pawn extends Piece {
             const currSquare: Square = board.findPiece(this);
 
             const color_dir = this.player == Player.WHITE ? 1 : -1;
+
+            Pawn.checkTake(color_dir, currSquare.row, currSquare.col, board, moves);
 
             if (Piece.inBounds(currSquare.row + color_dir, currSquare.col)) {
                 if (board.getPiece(new Square(currSquare.row + color_dir, currSquare.col)) != undefined)
