2f8ed60ee67686498997932f6925462fbf5e036a
Vlad Muntean <Vlad.Muntean@softwire.com>
2025-07-07 14:00:35 +0300

    Add moves for knight

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -8,6 +8,29 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+
+        const moves: Object[] = [];
+
+        try {
+            const currentSquare = board.findPiece(this);
+
+            const row = currentSquare.row;
+            const col = currentSquare.col;
+
+            const rowMoves: number[] = [-2, -1, 1, 2, -2, -1, 1, 2];
+            const colMoves: number[] = [1, 2, 2, 1, -1, -2, -2, -1];
+
+            for (let i = 0; i < rowMoves.length; i++) {
+                if (Piece.inBounds(row + rowMoves[i], col + colMoves[i]))
+                    moves.push({
+                        "row": row + rowMoves[i],
+                        "col": col + colMoves[i]
+                    })
+            }
+        } catch (e) {
+            console.error(e);
+        }
+
+        return moves;
     }
 }
